8234278e6c8629e87ca6367e5270af75ac0384c6
autenticacionrd <152920929+autenticacionrd@users.noreply.github.com>
2023-12-04 20:47:57 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
   apiKey: "AIzaSyCyTVl9C3zk-vJrHElu6cIKetgr8PDRMzU",
   authDomain: "ut50rnzd.firebaseapp.com",
   projectId: "ut50rnzd",
@@ -14,8 +9,6 @@ const firebaseConfig = {
   messagingSenderId: "661220333088",
   appId: "1:661220333088:web:63f2d5ccc5ba1821410e45",
   measurementId: "G-M1CN0JY9PH"
-};
+    });
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+    
